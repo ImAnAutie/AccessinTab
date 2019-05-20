4103e23e45ccd98e730214ae406d663d27d82295
Gregory Oakley-Stevenson <gregory@okonetwork.org.uk>
2019-05-20 15:41:23 +0100

    Removed pages folder, added check for setup and redirect to setup page if needs be.

--- www/js/app.js
+++ www/js/app.js
@@ -2,4 +2,16 @@ $(document).on("pagecontainershow", function (event, ui) {
         console.log(ui);
         pageid=ui.toPage[ui.toPage.length-1].id;
 	alert(pageid);
+	switch ($.mobile.activePage[0].id) {
+		case "page_main":
+			console.log("On main page, checking setup status");
+				if (localStorage.AccessinTABSetupComplete) {
+					console.log("AccessinTAB has been setup. Checking in with AccessinCommand");
+				} else {
+					console.log("AccessinTAB requires setup. Redirecting to setup page");
+					$.mobile.navigate("page_setup.html");
+				};
+			break;
+	};
+
 });
